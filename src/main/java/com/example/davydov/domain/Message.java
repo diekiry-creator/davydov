5ea412f23a5ac824fd0f1acef2ae6667520bf4a8
Davydov Kirill <kirillsonic@hotmail.com>
2020-01-13 01:33:48 +0300

    Микроразметка ​Open Graph

--- src/main/java/com/example/davydov/domain/Message.java
+++ src/main/java/com/example/davydov/domain/Message.java
@@ -49,4 +49,45 @@ public class Message {
     public void setCreationDate(LocalDateTime creationDate) {
         this.creationDate = creationDate;
     }
+
+    @JsonView(Views.FullMessage.class)
+    private String link;
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getLinkTitle() {
+        return linkTitle;
+    }
+
+    public void setLinkTitle(String linkTitle) {
+        this.linkTitle = linkTitle;
+    }
+
+    public String getLinkDescription() {
+        return linkDescription;
+    }
+
+    public void setLinkDescription(String linkDescription) {
+        this.linkDescription = linkDescription;
+    }
+
+    public String getLinkCover() {
+        return linkCover;
+    }
+
+    public void setLinkCover(String linkCover) {
+        this.linkCover = linkCover;
+    }
 }
--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -1,12 +1,12 @@
-
 <template>
     <v-card class="my-2">
         <v-card-text primary-title>
             <i>({{ message.id }})</i>
             {{ message.text }}
         </v-card-text>
+        <media v-if="message.link" :message="message"></media>
         <v-card-actions>
-            <v-btn value="Edit" @click="edit" small text rounded>Edit</v-btn>
+            <v-btn value="Edit" @click="edit" small flat round>Edit</v-btn>
             <v-btn icon @click="del" small>
                 <v-icon>delete</v-icon>
             </v-btn>
@@ -16,8 +16,10 @@
 
 <script>
     import { mapActions } from 'vuex'
+    import Media from 'components/media/Media.vue'
     export default {
         props: ['message', 'editMessage'],
+        components: { Media },
         methods: {
             ...mapActions(['removeMessageAction']),
             edit() {
